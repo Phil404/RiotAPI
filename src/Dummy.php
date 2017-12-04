c881338da6fd4f615a7a834ea34901b2611bf2ad
phil404 <philipp.96hh@hotmail.de>
2017-12-04 21:23:36 +0100

    testing coding styles

--- src/Dummy.php
+++ src/Dummy.php
@@ -5,8 +5,20 @@ namespace Phil404\RiotAPI;
 class Dummy
 {
 
+
+    /**
+     * Something is cool.
+     *
+     * @param $a
+     * @param $b
+     *
+     * @return mixed something
+     */
     public static function add($a, $b)
     {
-        return $a + $b;
-    }
+        return ($a + $b);
+
+    }//end add()
+
+
 }
