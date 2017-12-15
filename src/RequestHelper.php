6164bac0783a57ca83493a1c8194112b8cc9e416
phil404 <philipp.96hh@hotmail.de>
2017-12-15 19:13:28 +0100

    add some functions to create a request for riot api

--- src/RequestHelper.php
+++ src/RequestHelper.php
@@ -25,4 +25,32 @@ class RequestHelper
         }
         return $keyFile;
     }
+
+    public static function buildApiQuery(array $args)
+    {
+        if (array_key_exists("region", $args)
+            && array_key_exists("route", $args)
+            && array_key_exists("api_key", $args)) {
+            return "https://"
+                . $args['region']
+                . ".api.riotgames.com/"
+                . $args['route']
+                . "?api_key="
+                . $args['api_key'];
+        } else {
+            return null;
+        }
+    }
+
+    public static function convertArrayQueryReady(array $args)
+    {
+        $convertedArray = "";
+        foreach (array_keys($args) as $key) {
+            if ($args[$key] != "") {
+                if (strlen($convertedArray) > 0) $convertedArray .= "&";
+                $convertedArray .= $key . "=" . $args[$key];
+            }
+        }
+        return $convertedArray;
+    }
 }
--- test/RequestHelperTest.php
+++ test/RequestHelperTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use Phil404\RiotAPI\RequestHelper;
+use Phil404\RiotAPI\Models\Region;
 
 class RequestHelperTest extends TestCase
 {
@@ -30,5 +31,51 @@ class RequestHelperTest extends TestCase
     public function testGetApiKeyWithoutFile()
     {
         $this->assertNull(RequestHelper::getApiKey("apiKey.txt.test"));
+    }
+
+    public function testBuildApiQuery()
+    {
+        $array = [
+            "region" => Region::EUW,
+            "route" => "lol/test/shard-data",
+            "api_key" => "Test123"
+        ];
+        $expect = "https://" . Region::EUW .
+            ".api.riotgames.com/lol/test/shard-data?api_key=Test123";
+
+        $this->assertEquals($expect, RequestHelper::buildApiQuery($array));
+    }
+
+    public function testBuildApiQueryWithBrokenInformation()
+    {
+        $arrayWithoutRegion = ["route" => "lol/test", "api_key" => "nom"];
+        $arrayWithoutRoute = ["region" => Region::EUW, "api_key" => "nom"];
+        $arrayWithoutApiKey = ["region" => Region::EUW, "route" => "lol/test"];
+
+        $this->assertNull(RequestHelper::buildApiQuery($arrayWithoutRegion));
+        $this->assertNull(RequestHelper::buildApiQuery($arrayWithoutRoute));
+        $this->assertNull(RequestHelper::buildApiQuery($arrayWithoutApiKey));
+    }
+
+    public function testConvertQueryReady()
+    {
+        $array = ["test" => "A", "foo" => "bar", "random" => "number"];
+
+        $this->assertEquals(
+            "test=A&foo=bar&random=number",
+            RequestHelper::convertArrayQueryReady($array)
+        );
+    }
+
+    public function testConvertQueryReadyWithEmptyArray()
+    {
+        $this->assertEmpty(RequestHelper::convertArrayQueryReady([]));
+    }
+
+    public function testConvertQueryReadyWithEmptyValue()
+    {
+        $array = ["foo" => ""];
+
+        $this->assertEmpty(RequestHelper::convertArrayQueryReady($array));
     }
 }
