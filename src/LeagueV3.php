633dd27bc7bb590d6c629cc354155f4694d624f6
phil404 <philipp.96hh@hotmail.de>
2017-12-22 21:32:46 +0100

    #9 add some functions to 'LeagueV3'

--- src/LeagueV3.php
+++ src/LeagueV3.php
@@ -2,11 +2,82 @@
 
 namespace Phil404\RiotAPI;
 
+use Phil404\RiotAPI\Models\League\LeagueList;
 use Phil404\RiotAPI\Models\League\LeaguePosition;
 
 class LeagueV3
 {
     private static $_route = "lol/league/v3";
+
+    public static function getChallengerLeague(string $region,
+                                                    string $queue)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+        $data = $apiHandler->sendRequest(
+            LeagueV3::$_route
+            . "/challengerleagues/by-queue/"
+            . $queue
+        );
+
+        if (is_null($data)) {
+            return null;
+        } else {
+            return new LeagueList($data);
+        }
+    }
+
+    public static function getAllLeaguesForSummoner(string $region,
+                                                    int $summonerId)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+        $data = $apiHandler->sendRequest(
+            LeagueV3::$_route
+            . "/leagues/by-summoner/"
+            . $summonerId
+        );
+
+        if (is_null($data)) {
+            return null;
+        } else {
+            return new LeagueList($data);
+        }
+    }
+
+    public static function getLeagueById(string $region, string $leagueId)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+        $data = $apiHandler->sendRequest(
+            LeagueV3::$_route
+            . "/leagues/"
+            . $leagueId
+        );
+
+        if (is_null($data)) {
+            return null;
+        } else {
+            return new LeagueList($data);
+        }
+    }
+
+    public static function getMasterLeague(string $region, string $queue)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+        $data = $apiHandler->sendRequest(
+            LeagueV3::$_route
+            . "/masterleagues/by-queue/"
+            . $queue
+        );
+
+        if (is_null($data)) {
+            return null;
+        } else {
+            return new LeagueList($data);
+        }
+    }
 
     public static function getAllLeaguePositionsForSummoner(string $region,
                                                      int $summonerId)
@@ -19,12 +90,16 @@ class LeagueV3
             . $summonerId
         );
 
-        $qualifiedPositions = [];
-        foreach ($data as $datum) {
-            $qualifiedPositions[sizeof($qualifiedPositions)] =
-                new LeaguePosition($datum);
-        }
+        if (is_null($data)) {
+            return null;
+        } else {
+            $qualifiedPositions = [];
+            foreach ($data as $datum) {
+                $qualifiedPositions[sizeof($qualifiedPositions)] =
+                    new LeaguePosition($datum);
+            }
 
-        return $qualifiedPositions;
+            return $qualifiedPositions;
+        }
     }
 }
--- test/LeagueV3Test.php
+++ test/LeagueV3Test.php
@@ -2,6 +2,7 @@
 
 namespace Phil404\RiotAPI\Tests;
 
+use Phil404\RiotAPI\Models\League\LeagueList;
 use Phil404\RiotAPI\Models\League\LeaguePosition;
 use Phil404\RiotAPI\Models\Region;
 use Phil404\RiotAPI\LeagueV3;
@@ -10,6 +11,64 @@ use PHPUnit\Framework\TestCase;
 class LeagueV3Test extends TestCase
 {
     private $_summonerId = 25275660;
+
+    public function testGetChallengerLeague()
+    {
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("apiKey.txt");
+        } else {
+            $response = LeagueV3::getChallengerLeague(
+                Region::EUW,
+                "RANKED_SOLO_5x5"
+            );
+
+            self::assertTrue($response instanceof LeagueList);
+        }
+    }
+
+    public function testGetAllLeaguesForSummoner()
+    {
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("ApiKey not found!");
+        } else {
+            $response = LeagueV3::getAllLeaguesForSummoner(
+                Region::EUW,
+                $this->_summonerId
+            );
+
+            self::assertTrue($response instanceof LeagueList);
+        }
+    }
+
+    public function testGetLeagueById()
+    {
+        $leagueId = "b364c640-4180-30a3-b27e-2f03e1a1ac7a";
+
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("ApiKey not found!");
+        } else {
+            $response = LeagueV3::getLeagueById(
+                Region::EUW,
+                $leagueId
+            );
+
+            self::assertTrue($response instanceof LeagueList);
+        }
+    }
+
+    public function testGetMasterLeague()
+    {
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("ApiKey not found!");
+        } else {
+            $response = LeagueV3::getMasterLeague(
+                Region::EUW,
+                "RANKED_SOLO_5x5"
+            );
+
+            self::assertTrue($response instanceof LeagueList);
+        }
+    }
 
     public function testGetAllLeaguePositionsForSummoner()
     {
