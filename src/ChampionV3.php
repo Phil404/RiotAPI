34a55b9064c8a6d5a3516c055d893c9340055c0c
phil404 <philipp.96hh@hotmail.de>
2017-12-22 13:15:44 +0100

    #10 add 'getChampions' to 'ChampionV3'

--- src/ChampionV3.php
+++ src/ChampionV3.php
@@ -8,6 +8,18 @@ class ChampionV3
 {
     private static $_route = "lol/platform/v3/champions";
 
+    public static function getChampions(string $region)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+        $data = $apiHandler->sendRequest(ChampionV3::$_route)['champions'];
+        $collectedChampions = [];
+        for ($i = 0; $i < sizeof($data[0]); $i++) {
+            $collectedChampions[$i] = new Champion($data[$i]);
+        }
+        return $collectedChampions;
+    }
+
     public static function getChampionById(string $region, int $championId)
     {
         $apiHandler = new ApiHandler();
--- test/ChampionV3Test.php
+++ test/ChampionV3Test.php
@@ -9,6 +9,18 @@ use PHPUnit\Framework\TestCase;
 
 class ChampionV3Test extends TestCase
 {
+    public function testGetChampions()
+    {
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("ApiKey not found!");
+        } else {
+            $response = ChampionV3::getChampions(Region::EUW);
+
+            self::assertTrue(is_array($response));
+            self::assertTrue($response[0] instanceof Champion);
+        }
+    }
+
     public function testGetChampionById()
     {
         $championId = 412;
@@ -19,6 +31,7 @@ class ChampionV3Test extends TestCase
                 Region::EUW,
                 $championId
             );
+
             self::assertTrue($response instanceof Champion);
         }
     }
