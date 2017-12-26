a686163e525c0f742caacdbe4ef52aef341c35b3
phil404 <philipp.96hh@hotmail.de>
2017-12-26 11:39:44 +0100

    #6 finish 'CurrentGameParticipant'

--- src/Models/Spectator/CurrentGameParticipant.php
+++ src/Models/Spectator/CurrentGameParticipant.php
@@ -8,6 +8,48 @@ class CurrentGameParticipant
     private $_championId;
     private $_summonerName;
     private $_gameCustomizationObjects;
+    private $_bot;
+    private $_perks;
+    private $_spellTwoId;
+    private $_teamId;
+    private $_spellOneId;
+    private $_summonerId;
+
+    public function __construct(array $args = [])
+    {
+        if (array_key_exists("profileIconId", $args)) {
+            $this->_profileIconId = $args['profileIconId'];
+        }
+        if (array_key_exists("championId", $args)) {
+            $this->_championId = $args['championId'];
+        }
+        if (array_key_exists("summonerName", $args)) {
+            $this->_summonerName = $args['summonerName'];
+        }
+        if (array_key_exists("gameCustomizationObjects", $args)) {
+            $this->setGameCustomizationObjects(
+                $args['gameCustomizationObjects']
+            );
+        }
+        if (array_key_exists("bot", $args)) {
+            $this->_bot = $args['bot'];
+        }
+        if (array_key_exists("perks", $args)) {
+            $this->setPerks($args['perks']);
+        }
+        if (array_key_exists("spell2Id", $args)) {
+            $this->_spellTwoId = $args['spell2Id'];
+        }
+        if (array_key_exists("teamId", $args)) {
+            $this->_teamId = $args['teamId'];
+        }
+        if (array_key_exists("spell1Id", $args)) {
+            $this->_spellOneId = $args['spell1Id'];
+        }
+        if (array_key_exists("summonerId", $args)) {
+            $this->_summonerId = $args['summonerId'];
+        }
+    }
 
     /**
      * @param int $profileIconId
@@ -82,5 +124,105 @@ class CurrentGameParticipant
     public function getGameCustomizationObjects()
     {
         return $this->_gameCustomizationObjects;
+    }
+
+    /**
+     * @param boolean $bot
+     */
+    public function setBot(bool $bot)
+    {
+        $this->_bot = $bot;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isBot()
+    {
+        return $this->_bot;
+    }
+
+    /**
+     * @param array|Perks $perks
+     */
+    public function setPerks($perks)
+    {
+        if ($perks instanceof Perks) {
+            $this->_perks = $perks;
+        } else {
+            $this->_perks = new Perks($perks);
+        }
+    }
+
+    /**
+     * @return Perks
+     */
+    public function getPerks()
+    {
+        return $this->_perks;
+    }
+
+    /**
+     * @param int $spellTwoId
+     */
+    public function setSpell2Id(int $spellTwoId)
+    {
+        $this->_spellTwoId = $spellTwoId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpell2Id()
+    {
+        return $this->_spellTwoId;
+    }
+
+    /**
+     * @param int $teamId
+     */
+    public function setTeamId(int $teamId)
+    {
+        $this->_teamId = $teamId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTeamId()
+    {
+        return $this->_teamId;
+    }
+
+    /**
+     * @param int $spellOneId
+     */
+    public function setSpell1Id(int $spellOneId)
+    {
+        $this->_spellOneId = $spellOneId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpell1Id()
+    {
+        return $this->_spellOneId;
+    }
+
+    /**
+     * @param int $summonerId
+     */
+    public function setSummonerId(int $summonerId)
+    {
+        $this->_summonerId = $summonerId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSummonerId()
+    {
+        return $this->_summonerId;
     }
 }
--- test/Models/Spectator/CurrentGameParticipantTest.php
+++ test/Models/Spectator/CurrentGameParticipantTest.php
@@ -3,11 +3,45 @@
 namespace Phil404\RiotAPI\Tests\Models\Spectator;
 
 use Phil404\RiotAPI\Models\Spectator\GameCustomizationObject;
+use Phil404\RiotAPI\Models\Spectator\Perks;
 use PHPUnit\Framework\TestCase;
 use Phil404\RiotAPI\Models\Spectator\CurrentGameParticipant;
 
 class CurrentGameParticipantTest extends TestCase
 {
+    public function testConstructor()
+    {
+        $object = new CurrentGameParticipant(
+            [
+                "profileIconId" => 1,
+                "championId" => 2,
+                "summonerName" => "foo",
+                "gameCustomizationObjects" => [new GameCustomizationObject()],
+                "bot" => true,
+                "perks" => new Perks,
+                "spell2Id" => 3,
+                "teamId" => 100,
+                "spell1Id" => 4,
+                "summonerId" => 5
+            ]
+        );
+
+        self::assertEquals(1, $object->getProfileIconId());
+        self::assertEquals(2, $object->getChampionId());
+        self::assertEquals("foo", $object->getSummonerName());
+        self::assertTrue(is_array($object->getGameCustomizationObjects()));
+        self::assertTrue(
+            $object->getGameCustomizationObjects()[0]
+            instanceof GameCustomizationObject
+        );
+        self::assertTrue($object->isBot());
+        self::assertTrue($object->getPerks() instanceof Perks);
+        self::assertEquals(3, $object->getSpell2Id());
+        self::assertEquals(100, $object->getTeamId());
+        self::assertEquals(4, $object->getSpell1Id());
+        self::assertEquals(5, $object->getSummonerId());
+    }
+
     public function testSetAndGetProfileIconId()
     {
         $object = new CurrentGameParticipant();
@@ -64,5 +98,77 @@ class CurrentGameParticipantTest extends TestCase
         $object->setGameCustomizationObjects([new GameCustomizationObject()]);
 
         self::assertEquals(1, sizeof($object->getGameCustomizationObjects()));
+    }
+
+    public function testSetAndIsBot()
+    {
+        $object = new CurrentGameParticipant();
+        $object->setBot(true);
+
+        self::assertTrue($object->isBot());
+
+        $object->setBot(false);
+
+        self::assertFalse($object->isBot());
+    }
+
+    public function testSetAndGetPerks()
+    {
+        $object = new CurrentGameParticipant();
+        $object->setPerks(new Perks());
+
+        self::assertTrue($object->getPerks() instanceof Perks);
+
+        $object->setPerks(["perkStyle" => 10]);
+
+        self::assertTrue($object->getPerks() instanceof Perks);
+    }
+
+    public function testSetAndGetSpell2Id()
+    {
+        $object = new CurrentGameParticipant();
+        $object->setSpell2Id(1);
+
+        self::assertEquals(1, $object->getSpell2Id());
+
+        $object->setSpell2Id(2);
+
+        self::assertEquals(2, $object->getSpell2Id());
+    }
+
+    public function testSetAndGetTeamId()
+    {
+        $object = new CurrentGameParticipant();
+        $object->setTeamId(100);
+
+        self::assertEquals(100, $object->getTeamId());
+
+        $object->setTeamId(200);
+
+        self::assertEquals(200, $object->getTeamId());
+    }
+
+    public function testSetAndGetSpell1Id()
+    {
+        $object = new CurrentGameParticipant();
+        $object->setSpell1Id(1);
+
+        self::assertEquals(1, $object->getSpell1Id());
+
+        $object->setSpell1Id(2);
+
+        self::assertEquals(2, $object->getSpell1Id());
+    }
+
+    public function testSetAndGetSummonerId()
+    {
+        $object = new CurrentGameParticipant();
+        $object->setSummonerId(1);
+
+        self::assertEquals(1, $object->getSummonerId());
+
+        $object->setSummonerId(2);
+
+        self::assertEquals(2, $object->getSummonerId());
     }
 }
