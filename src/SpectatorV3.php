305b94fff58b313590238fdb5f6aed83836bae9e
phil404 <philipp.96hh@hotmail.de>
2017-12-28 11:48:34 +0100

    #6 add 'getCurrentGameInfoBySummoner' to 'SpectatorV3'

--- src/SpectatorV3.php
+++ src/SpectatorV3.php
@@ -2,11 +2,31 @@
 
 namespace Phil404\RiotAPI;
 
+use Phil404\RiotAPI\Models\Spectator\CurrentGameInfo;
 use Phil404\RiotAPI\Models\Spectator\FeaturedGames;
 
 class SpectatorV3
 {
     private static $_route = "lol/spectator/v3";
+
+    public static function getCurrentGameInfoBySummoner(string $region,
+                                                        int $summonerId)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+
+        $data = $apiHandler->sendRequest(
+            SpectatorV3::$_route
+            . "/active-games/by-summoner/"
+            . $summonerId
+        );
+
+        if (is_null($data)) {
+            return null;
+        } else {
+            return new CurrentGameInfo($data);
+        }
+    }
 
     public static function getFeaturedGames(string $region)
     {
--- test/SpectatorV3Test.php
+++ test/SpectatorV3Test.php
@@ -3,12 +3,37 @@
 namespace Phil404\RiotAPI\Tests;
 
 use Phil404\RiotAPI\Models\Region;
+use Phil404\RiotAPI\Models\Spectator\CurrentGameInfo;
 use Phil404\RiotAPI\Models\Spectator\FeaturedGames;
+use Phil404\RiotAPI\Models\Summoner\Summoner;
 use Phil404\RiotAPI\SpectatorV3;
+use Phil404\RiotAPI\SummonerV3;
 use PHPUnit\Framework\TestCase;
 
 class SpectatorV3Test extends TestCase
 {
+    public function testGetCurrentGameInfoBySummoner()
+    {
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("ApiKey not found!");
+        } else {
+            $featuredGame = SpectatorV3::getFeaturedGames(Region::EUW);
+            $summonerName = $featuredGame->getGameList()[0]
+                ->getParticipants()[0]
+                ->getSummonerName();
+            $summoner = SummonerV3::getSummonerByName(Region::EUW, $summonerName);
+            $response = SpectatorV3::getCurrentGameInfoBySummoner(
+                Region::EUW,
+                $summoner->getId()
+            );
+
+            self::assertTrue(
+                $response instanceof CurrentGameInfo
+            );
+            self::assertNotNull($response->getGameId());
+        }
+    }
+
     public function testGetFeaturedGames()
     {
         if (!file_exists("apiKey.txt")) {
