b088b03d5baa9300b2a3c545d5c6ea67bb995ddb
phil404 <philipp.96hh@hotmail.de>
2018-01-03 14:59:59 +0100

    #8 add 'getSummonerSpellById' to 'LoLStaticDataV3'

--- src/LoLStaticDataV3.php
+++ src/LoLStaticDataV3.php
@@ -2,9 +2,43 @@
 
 namespace Phil404\RiotAPI;
 
+use Phil404\RiotAPI\Models\StaticData\SummonerSpell;
+
 class LoLStaticDataV3
 {
     private static $_route = "/lol/static-data/v3/";
+
+    public static function getSummonerSpellById(string $region,
+                                                int $id,
+                                                array $parameters)
+    {
+        $apiHandler = new ApiHandler();
+        $apiHandler->setRegion($region);
+
+        if (!empty($parameters['tags'])) {
+            $forQuery['tags'] = $parameters['tags'];
+        } else {
+            $forQuery['tags'] = "all";
+        }
+        if (!empty($parameters['locale'])) {
+            $forQuery["locale"] = $parameters['locale'];
+        }
+        if (!empty($parameters['version'])) {
+            $forQuery["version"] = $parameters['version'];
+        }
+
+        $data = $apiHandler->sendRequest(
+            LoLStaticDataV3::$_route
+            . "summoner-spells/"
+            . $id,
+            $forQuery
+        );
+
+        if (!is_null($data)) {
+            $data = new SummonerSpell($data);
+        }
+        return $data;
+    }
 
     public static function getVersions(string $region)
     {
--- test/LoLStaticDataV3Test.php
+++ test/LoLStaticDataV3Test.php
@@ -4,10 +4,27 @@ namespace Phil404\RiotAPI\Tests;
 
 use Phil404\RiotAPI\LoLStaticDataV3;
 use Phil404\RiotAPI\Models\Region;
+use Phil404\RiotAPI\Models\StaticData\SummonerSpell;
 use PHPUnit\Framework\TestCase;
 
 class LoLStaticDataV3Test extends TestCase
 {
+    public function testGetSummonerSpellById()
+    {
+        if (!file_exists("apiKey.txt")) {
+            self::markTestSkipped("ApiKey not found!");
+        } else {
+            $response = LoLStaticDataV3::getSummonerSpellById(
+                Region::EUW,
+                4,
+                ["locale" => "en_US"]
+            );
+
+            self::assertTrue($response instanceof SummonerSpell);
+            self::assertNotNull($response->getId());
+        }
+    }
+
     public function testGetVersions()
     {
         if (!file_exists("apiKey.txt")) {
